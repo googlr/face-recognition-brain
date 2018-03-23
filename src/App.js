d587a68d21f0fe4f04aefdfa462e268eab0e5367
googlr <xg626@nyu.edu>
2018-03-23 16:21:30 -0400

    minor bugs fixed

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
-    if( route == 'signout' ){
+    if( route === 'signout' ){
       this.setState({isSignedIn: false});
     } else if( route === 'home' ){
       this.setState({isSignedIn: true});
@@ -111,7 +111,7 @@ class App extends Component {
         ? <div>
             <Rank />
           </div>
-        : ( this.state.route == 'signin'
+        : ( this.state.route === 'signin'
             ? <SignIn onRouteChange={this.onRouteChange} />
             : <Register onRouteChange={this.onRouteChange} />
         )
